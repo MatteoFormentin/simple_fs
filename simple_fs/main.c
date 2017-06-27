4ebf8e72b95b236a4e4477a48d25042f1eb3fcb0
Matteo Formentin <matteo.formentin@icloud.com>
2017-06-27 19:32:10 +0200

    Initial Commit

---
+++ simple_fs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simple_fs
+//
+//  Created by Matteo Formentin on 27/06/17.
+//  Copyright © 2017 Matteo Formentin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
