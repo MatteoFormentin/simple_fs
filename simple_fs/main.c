2017fd5efb78da6396bd29a7bc2f105e6d7e032c
Matteo Formentin <matteo.formentin@icloud.com>
2017-06-28 22:59:15 +0200

    Creata lettura input

--- simple_fs/main.c
+++ simple_fs/main.c
@@ -7,9 +7,161 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+
+int check_path_format(char * path);
+char * check_content_format(char content[]);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    
+    //Lettura Comando, Percorso, Contenuto e Inserimento in Array Corrisponente
+    char temp[50];
+    char * command;
+    char * path;
+    char * content;
+    printf("inserire comando:\n ");
+    gets(temp);
+    command=strtok(temp," ");
+    path=strtok(NULL," ");
+    content=strtok(NULL," ");
+
+   
+    //Esecuzione del comando corretto
+    
+    
+    //CREATE
+    if(strcmp(command, "create")==0)
+    {
+        printf("comando: create\n");
+        if(check_path_format(path)==1)
+        {
+            //Operazione Create
+            printf("percorso ok\n");
+        }
+    }
+    
+    //CREATE_DIR
+    else if(strcmp(command, "create_dir")==0)
+    {
+        printf("comando: create_dir");
+        if(check_path_format(path)==1)
+        {
+            //Operazione create_dir
+            printf("percorso ok");
+
+        }
+    }
+    
+    //READ
+    else if(strcmp(command, "read")==0)
+    {
+        printf("comando: read");
+        if(check_path_format(path)==1)
+        {
+            //Operazione read
+        }
+    }
+    
+    //WRITE
+    else if(strcmp(command, "write")==0)
+    {
+        printf("comando: write");
+        if(check_path_format(path)==1)
+        {
+            content = check_content_format(content);
+            printf("\nc:  %s\n", content);
+            
+        }
+    }
+    
+    //DELETE
+    else if(strcmp(command, "delete")==0)
+    {
+        printf("comando: delete");
+        if(check_path_format(path)==1)
+        {
+            //Operazione delete
+        }
+    }
+    
+    //DELETE_R
+    else if(strcmp(command, "delete_r")==0)
+    {
+        printf("comando: delete_r");
+        if(check_path_format(path)==1)
+        {
+            //Operazione delete_r
+        }
+    }
+    
+    //FIND
+    else if(strcmp(command, "find")==0)
+    {
+        printf("comando: find");
+        if(check_path_format(path)==1)
+        {
+            //Operazione find
+        }
+    }
+    
+    //EXIT
+    else if(strcmp(command, "exit")==0)
+    {
+        printf("comando: exit");
+        return 1;
+    }
+    
+    //ERRORE INPUT COMANDO
+    else
+    {
+        printf("comando non valido");
+    }
+
 }
+
+
+//FUNZIONI CONTROLLO CORRETTEZZA INPUT - IMPORTANTE: CONTROLLARE SE SERVE ESCLUDERE INPUT CON PATH O CONTENUTO DOVE NON PREVISTO
+
+int check_path_format(char path[])
+{
+    if(path[0]!='/' || path==NULL)
+    {
+        printf("\nerrore nel formato del percorso!\n");
+        return -1;
+    }
+    printf("\npercorso ok\n");
+    return 1;
+}
+
+//Controlla se l'ingresso ha gli apici e li rimuove
+char * check_content_format(char content[])
+{
+    //controlla lunghezza array
+    int last_element=0;
+    while(content[last_element]!='\0')
+    {
+        last_element++;
+    }
+    last_element--;
+    
+    //controlla gli apici
+    if(content[0]!='"' || content==NULL || content[last_element]!='"')
+    {
+        printf("\nerrore nel formato del contenuto!\n");
+        return 'a';
+    }
+    else //rimuovi gli apici
+    {
+        printf("\ncontenuto ok\n");
+        char string_without_apices[last_element];
+        int i=1;
+        while(i<last_element)
+        {
+            string_without_apices[i-1]=content[i];
+            i++;
+        }
+        return string_without_apices;
+    }
+}
+
+
